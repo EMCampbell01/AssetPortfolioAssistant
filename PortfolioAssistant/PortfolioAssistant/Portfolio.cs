7e5d0789b108d86bf56b607ff08172745ed9efc4
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2021-09-19 20:12:05 +0100

    Update Portfolio.cs

--- PortfolioAssistant/PortfolioAssistant/Portfolio.cs
+++ PortfolioAssistant/PortfolioAssistant/Portfolio.cs
@@ -1,1 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
 
+namespace PortfolioAssistant
+{
+    class Portfolio
+    {
+        public ArrayList holdings = new ArrayList();
+    }
+}
