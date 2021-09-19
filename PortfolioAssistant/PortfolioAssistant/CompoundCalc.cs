a1cf935daca30e4b983aa90cea739529d2a0d35a
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2021-09-19 20:06:37 +0100

    Update CompoundCalc.cs

--- PortfolioAssistant/PortfolioAssistant/CompoundCalc.cs
+++ PortfolioAssistant/PortfolioAssistant/CompoundCalc.cs
@@ -7,6 +7,15 @@ namespace PortfolioAssistant
 {
     public static class CompoundCalc
     {
+        /*
+         * Compund calculates, displays and returns the anual growth of an investment
+         * Parametrs : 
+         * inital       = starting value of investment at year 0
+         * growth       = anual intrest earned on investment
+         * duration     = lifespan of investment (years)
+         * contribution = value of anual investment contribution added
+         * Method raturns yearly value of investment
+         */
         public static ArrayList Compound(double initial, double growth, int duration, double contribution)
         {
             ArrayList value_list = new ArrayList();
@@ -14,8 +23,8 @@ namespace PortfolioAssistant
             int current_year = 0;
             double current_value = initial;
 
-            double total_invested = initial;
-            double total_gain = 0;
+            double total_invested = initial; //total value of all investment deposits
+            double total_gain = 0;           //total value of interest earned on investment
 
             while(current_year < duration)
             {
@@ -29,6 +38,7 @@ namespace PortfolioAssistant
 
                 current_value += contribution;
                 total_invested += contribution;
+                total_gain = current_value - total_invested;
 
             }
             
